8492b300f49186a239653f783d17481ddd77f0e3
David Liu <a.davidliu@gmail.com>
2016-09-24 23:08:37 -0700

    [wm] get focusing and layering mostly right

--- bin/focus.sh
+++ bin/focus.sh
@@ -6,34 +6,45 @@
 
 cur=$(pfw)
 
+# TODO: store focus history or something...
+# This is just a hack to get a semblance of ordering.
+list_windows() {
+  wmctrl -l | cut -d' ' -f1 | while read -r wid; do
+    wattr m "$wid" && printf "%s\n" "$wid"
+  done
+}
+
+# TODO: store focus history or something...
 case $1 in
-  next) wid=$(lsw | grep -A1 "$cur" | grep -v "$cur" || lsw | head -n1) ;;
-  prev) wid=$(lsw | grep -B1 "$cur" | grep -v "$cur" || lsw | tail -n1) ;;
+  next) wid=$(list_windows | grep -A1 "$cur" | grep -v "$cur" || list_windows | head -1) ;;
+  prev) wid=$(list_windows | grep -B1 "$cur" | grep -v "$cur" || list_windows | tail -1) ;;
   0x*) wattr "$1" && wid=$1 ;;
 esac
 
-# Move mouse cursor to center if there are no windows to focus.
-if [ -z "$wid" ]; then
+if [ -z "$(lsw)" ]; then
   read -r w h << EOF
   $(wattr wh "$(lsw -r)")
 EOF
   wmp -a $((w/2)) $((h/2))
   exit
+elif [ "$wid" = "$cur" ]; then
+  exit
 fi
-
-case $(xwinfo -c "$wid") in
-  chromium)
-    ;;
-  *)
-    chwso -r "$wid"
-    ;;
-esac
 
 wtf "$wid"
 
-#read -r x y w h << EOF
-#$(wattr xywh "$wid")
-#EOF
-#
-#wmp -a "$x" "$y"
-#wmp -r $((w-1)) $((h-1))
+read -r x y w h << EOF
+$(wattr xywh "$wid")
+EOF
+
+case $(xwinfo -c "$wid") in
+  chromium)
+    chwso -l "$wid"
+    restack
+    wmp -a $((x + w)) $((y + h))
+    ;;
+  *)
+    chwso -r "$wid"
+    wmp -a $((x + w / 2)) $((y + h / 2))
+    ;;
+esac
